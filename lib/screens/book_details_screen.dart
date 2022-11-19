680db5b3540de1c4079ba4afcc0878a9fe2eb0b2
kocakaysegul <aysegulkocak112@gmail.com>
2022-11-19 13:26:24 +0300

    library-app completed

---
+++ lib/screens/book_details_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:library_app/model/book.dart';
+
+class BookDetailsScreen extends StatelessWidget {
+  final Book book;
+
+  BookDetailsScreen(this.book);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(book.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                book.imageUrl,
+                height: 50,
+                width: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  book.year.toString(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 17.0, fontStyle: FontStyle.italic),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  book.description,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 22.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/book_list_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:library_app/model/book.dart';
+import 'package:library_app/screens/book_details_screen.dart';
+
+class BookListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'LIBRARY',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          leading: Icon(Icons.bookmark, size: 30),
+        ),
+        body: ListView.builder(
+            itemCount: bookList.length,
+            itemBuilder: (context, index) {
+              Book book = bookList[index];
+              return Card(
+                elevation: 5,
+                shadowColor: Theme.of(context).primaryColor,
+                child: ListTile(
+                  title: Text(book.title),
+                  subtitle: Text(book.year.toString()),
+                  leading: Image.network(book.imageUrl),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  onTap: () {
+                    showModalBottomSheet(
+                        barrierColor: Theme.of(context)
+                            .primaryColorLight
+                            .withOpacity(0.3),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        elevation: 5,
+                        context: context,
+                        builder: (context) => BookDetailsScreen(book));
+                  },
+                ),
+              );
+            }));
+  }
+}
